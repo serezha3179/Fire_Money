74102c145d8bafba5659e3bb7e7fcb7bfa8ce534
serezha3179 <sergioss31@rambler.ru>
2022-06-12 13:38:44 +0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import * as flsFunctions from './modules/functions.js';
+import './script.js';
+
+flsFunctions.testWebP(function (support) {
+    
+    if (support == true) {
+    document.querySelector('body').classList.add('webp');
+    }else{
+    document.querySelector('body').classList.add('no-webp');
+    }
+}); 
+
+
