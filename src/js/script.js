b1ec1e5d14f25dbd44eea7ba0556c9fdc07e0da2
Uladzimir Abramkin <abramkin@wiztechgroup.com>
2022-07-06 21:14:28 +0300

    FAQ: fixed height for active item

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,31 @@
 
-        let faq = document.querySelectorAll('.FAQ__item');
-        faq.forEach(item => {
-            item.addEventListener('click',() => {
-                if (!item.classList.contains('active')) {
-                    faq.forEach(item => {
-                      let a = item.querySelector('.FAQ__item-text')
-                      item.classList.remove('active')
-                    a.style.maxHeight = null;
-                  })
-                     item.classList.add('active')
-                }else {
-                     item.classList.remove('active');
-                }
-                let submenu = item.querySelector('.FAQ__item-text')   
-            if (submenu.style.maxHeight) {
-              submenu.style.maxHeight = null;
-              
-            } else {
-              submenu.style.maxHeight = submenu.scrollHeight + "px";
-              
-            }
-        })
-        })
+let faq = document.querySelectorAll('.FAQ__item');
+let faqActiveInit = document.querySelector('.FAQ__item-text');
+faqActiveInit.style.maxHeight = faqActiveInit.clientHeight + "px";
 
+
+faq.forEach(item => {
+  item.addEventListener('click',() => {
+        if (!item.classList.contains('active')) {
+            faq.forEach(item => {
+              let a = item.querySelector('.FAQ__item-text')
+              item.classList.remove('active')
+            a.style.maxHeight = null;
+          })
+              item.classList.add('active')
+        }else {
+              item.classList.remove('active');
+        }
+        let submenu = item.querySelector('.FAQ__item-text')   
+    if (submenu.style.maxHeight) {
+      submenu.style.maxHeight = null;
+      
+    } else {
+      submenu.style.maxHeight = submenu.scrollHeight + "px";
+      
+    }
+    });
+});
 
      
        
